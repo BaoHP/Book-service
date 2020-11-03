9f88db64e11f0385a992b73cdb61d17c0316d1e6
Hua Phu Bao <bao.hp@gnt.com.vn>
2020-11-03 16:36:01 +0700

    #3 cm call api

--- books/app/controllers/api/v1/books_controller.rb
+++ books/app/controllers/api/v1/books_controller.rb
@@ -4,7 +4,11 @@ class Api::V1::BooksController < ActionController::API
     books = books.map do |book|
       { id: book.id, store_id: book.store_id, name: book.name }
     end
-    
+    response = HTTParty.get('http://localhost:3000/api/v1/stores')
+    puts "XXXXXXXXXXXX"
+    puts eval(response.body)[:results].first[:id]
+    puts "XXXXXXXXXXXX"
+
     render json: { results: books }.to_json, status: :ok
   end
 end
